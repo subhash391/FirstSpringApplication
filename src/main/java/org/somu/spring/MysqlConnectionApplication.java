9074d921f7ef11e0758a090ffbdbce1e30b1d36e
subhashBugFixes <subhash.391@gmail.com>
2022-07-31 01:18:46 +0530

    FirstSpringApplication-Only with Main Class.

---
+++ src/main/java/org/somu/spring/MysqlConnectionApplication.java
@@ -0,0 +1,11 @@
+package org.somu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlConnectionApplication.class, args);
+    }
+}
